d04211376adca9768ff0e239f6201e78091dd78b
Banja David Lotigo <innocdavid@gmail.com>
2024-06-13 13:12:12 +0300

    clean header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useAppContext } from "../contexts/AppContext";
+import LogoutButton from "./LogoutButton";
 
 const Header = () => {
     const { isLoggedIn } = useAppContext();
@@ -15,7 +16,7 @@ const Header = () => {
                         <>
                             <Link to="/my-bookings">My Bookings</Link>
                             <Link to="/my-hotels">My Hotels</Link>
-                            <button>Logout</button>
+                            <LogoutButton />
                         </>
                         ) : (
                             <Link
