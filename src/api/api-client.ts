5d40b0a4be6293c10081bbf0abd44541194ba4b1
Banja David Lotigo <innocdavid@gmail.com>
2024-06-13 12:16:13 +0300

    add login hook

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -1,3 +1,4 @@
+import { LoginFormData } from "../pages/Login";
 import { RegisterFormData } from "../pages/Register";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -17,6 +18,23 @@ export const register  = async (formData: RegisterFormData) => {
     if (!response.ok) {
         throw new Error(responseBody.message);
     }
+};
+
+export const login = async (formData: LoginFormData) => {
+    const response = await fetch(`${API_BASE_URL}/api/v1/auth/login`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    });
+
+    const responseBody = await response.json();
+
+    if (!response.ok) throw new Error(responseBody.message);
+
+    return responseBody;
 };
 
 export const validateToken = async () => {
